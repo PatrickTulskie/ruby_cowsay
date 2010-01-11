3259c1e80146f0aedb4e39de23a0c62729a74d67
patricktulskiem <patricktulskie@gmail.com>
2010-01-10 22:39:06 -0500

    Setting up the gem to be able to run tests.

---
+++ tests/test_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+# Not sure if I want to use test/unit with shoulda or if I want to use RSpec yet...
+# require 'test/unit'
+# require 'shoulda'
+# require 'spec'
+require File.dirname(__FILE__) + '/../lib/ruby_cowsay'
